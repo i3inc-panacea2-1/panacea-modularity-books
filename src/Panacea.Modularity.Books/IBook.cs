8dcec98ab566cdafe66344676b1dfe0a89056467
Portno <j.remoundos@dotbydot.gr>
2019-07-08 12:36:37 +0300

    first commit

---
+++ src/Panacea.Modularity.Books/IBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Books
+{
+    public interface IBook
+    {
+        string Language {get;set; }
+
+        string Writer { get; set; }
+
+        string Year { get; set; }
+
+        string Isbn { get; set; }
+
+        string Img { get; set; }
+
+        string Description { get; set; }
+
+        List<IDataUrl> DataUrl { get; set; }
+
+    }
+
+    public class IDataUrl
+    {
+        string Url { get; set; }
+
+        string DataType { get; set; }
+    }
+}
---
+++ src/Panacea.Modularity.Books/IBooksPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Books
+{
+    public interface IBooksPlugin : IPlugin
+    {
+        void OpenBook(IBook book, string pluginName);
+    }
+}
